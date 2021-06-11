25e90a81ed548b3c5d5c6b0ed0b894cf6616381a
tejusasapu1247 <67572528+tejusasapu1247@users.noreply.github.com>
2021-06-11 14:02:08 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,15 +1,25 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.28.0"
+#     }
+
+#     random = {
+#       source  = "hashicorp/random"
+#       version = "3.0.0"
+#     }
+#   }
+
+#   required_version = ">= 0.14.0"
+# }
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+      version = "~> 3.27"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 0.14.9"
 }
