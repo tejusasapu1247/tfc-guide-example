b08ee6685098134f822fbfeae0f76bf7a51750ab
tejusasapu1247 <67572528+tejusasapu1247@users.noreply.github.com>
2021-06-11 13:55:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,46 @@
-provider "aws" {
-  region = var.aws_region
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+# provider "random" {}
+
+# resource "random_pet" "table_name" {}
+
+# resource "aws_dynamodb_table" "tfc_example_table" {
+#   name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+#   read_capacity  = var.db_read_capacity
+#   write_capacity = var.db_write_capacity
+#   hash_key       = "UUID"
+
+#   attribute {
+#     name = "UUID"
+#     type = "S"
+#   }
+# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
 }
 
-provider "random" {}
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
 
-resource "random_pet" "table_name" {}
+resource "aws_instance" "app_server" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
+  tags = {
+    Name = var.instance_name
   }
 }
+
+
